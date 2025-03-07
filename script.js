1df9e88966c45ae8661fe041283bffb255e2daa8
atento <96808973+beansonbread@users.noreply.github.com>
2025-03-07 11:40:41 -0500

    add init form structure and form validation

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+/*projects section*/
 document.querySelectorAll(".details").forEach(button => {
     button.addEventListener("click", showDetails) 
 });
         
 function showDetails(){
-    let projectId = this.getAttribute("data-target");
-    let description = document.getElementById(projectId);
+    const projectId = this.getAttribute("data-target");
+    const description = document.getElementById(projectId);
 
     if (description.style.display === "none") {
         description.style.display = "block";
@@ -12,5 +13,24 @@ function showDetails(){
     } else {
         description.style.display = "none";
         this.textContent = "Show Details";
+    }
+}
+
+/*contact section*/
+function validateForm() {
+    const name = document.forms["contactForm"]["name"].value;
+    const email = document.forms["contactForm"]["email"].value
+    const message = document.forms["contactForm"]["message"].value
+    if (name == "") {
+        alert("Name must be filled out");
+        return false;
+    } 
+    if (email =="") {
+        alert ("Please provide an email address");
+        return false;
+    }
+    if (message =="") {
+        alert ("Please enter a short message");
+        return false;
     }
 }
